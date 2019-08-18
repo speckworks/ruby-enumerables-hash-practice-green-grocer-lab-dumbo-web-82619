6f6fb93c3cef2fe9f06e8f6bc79fd034ed10ed2b
John Karrell Speck <speckworks@gmail.com>
2019-08-18 16:31:14 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,9 +1,30 @@
+ # # let(:items) do
+  #   [
+  #     {"AVOCADO" => {:price => 3.00, :clearance => true}},
+  #     {"KALE" => {:price => 3.00, :clearance => false}},
+  #     {"BLACK_BEANS" => {:price => 2.50, :clearance => false}},
+  #     {"ALMONDS" => {:price => 9.00, :clearance => false}},
+  #     {"TEMPEH" => {:price => 3.00, :clearance => true}},
+  #     {"CHEESE" => {:price => 6.50, :clearance => false}},
+  #     {"BEER" => {:price => 13.00, :clearance => false}},
+  #     {"PEANUTBUTTER" => {:price => 3.00, :clearance => true}},
+  #     {"BEETS" => {:price => 2.50, :clearance => false}},
+  #     {"SOY MILK" => {:price => 4.50, :clearance => true}}
+  #   ]
+  # end
+   
+# bands.reduce({}) do |memo, pair|
+#   p memo # First block parameter
+#   p pair # Second block parameter
+#   memo # Return value for the block. It becomes the memo in the next go-round
 def consolidate_cart(cart)
-  # code here
+    hash = {}
+    :items.reduce({}) do |:price, :count|
 end
 
+
 def apply_coupons(cart, coupons)
-  # code here
+  # 
 end
 
 def apply_clearance(cart)
