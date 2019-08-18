162d10e775969d8f4408071ab2d24f37079f0431
John Karrell Speck <speckworks@gmail.com>
2019-08-18 18:56:02 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -17,7 +17,7 @@ def consolidate_cart(cart)
     new.cart = {}
     cart.each do |items_array|
      items_array.each do |item, attribute_hash|
-      new_cart[item] || = attribute_hash
+      new_cart[item] ||= attribute_hash
       new_cart[item][:count] ? new_cart[item][:count] += 1 :   
       new_cart[item][:count] = 1
     end
