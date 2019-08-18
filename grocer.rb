912b4038622cbde275a73980b21650ea25dd211f
John Karrell Speck <speckworks@gmail.com>
2019-08-18 19:00:31 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -23,7 +23,25 @@ def consolidate_cart(cart)
   end 
 end 
 new_cart 
+def apply_coupons(cart, coupons) 
+  coupons.each do |coupon| 
+    coupon.each do |attribute, value| 
+      name = coupon[:item] 
+      if cart[name] && cart[name][:count] >= coupon[:num] 
+        if cart["#{name} W/COUPON"] 
+          cart["#{name} W/COUPON"][:count] += 1 
+        else 
+          cart["#{name} W/COUPON"] = {:price => coupon[:cost], 
+          :clearance => cart[name][:clearance], :count => 1} 
+        end 
+      cart[name][:count] -= coupon[:num] 
+    end 
+  end 
+end 
+  cart 
 end
+end
+
 
 # def apply_coupons(cart, coupons)
 #   if :item[] 
