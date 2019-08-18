4d5df1e52488daca02bf66e61e7bb718fcee5848
John Karrell Speck <speckworks@gmail.com>
2019-08-18 18:50:48 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -17,6 +17,7 @@ def consolidate_cart(cart)
     new.cart = {}
     cart.each do |items_array|
      items_array.each do |item, attribute_hash|
+      new_cart[item] = 
 end
 
 def apply_coupons(cart, coupons)
