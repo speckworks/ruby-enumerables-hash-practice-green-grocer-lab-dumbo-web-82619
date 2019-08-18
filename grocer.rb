ad895c25a827a8a61ac7eeae2dc7a6f7eadea612
John Karrell Speck <speckworks@gmail.com>
2019-08-18 19:15:09 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -30,7 +30,7 @@ def apply_coupons(cart, coupons)
     coupon.each do |attribute, value| 
       name = coupon[:item] 
       if cart[name] && cart[name][:count] >= coupon[:num] 
-        if cart["#{name} W/COUPON"] 
+        if cart["#{name} W/COUPON"]
           cart["#{name} W/COUPON"][:count] += 1 
         else 
           cart["#{name} W/COUPON"] = {:price => coupon[:cost], 
