1eadece159f557b01f810deed4c5776c77720312
John Karrell Speck <speckworks@gmail.com>
2019-08-18 16:40:30 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,18 +1,18 @@
 require pry
- # # let(:items) do
-  #   [
-  #     {"AVOCADO" => {:price => 3.00, :clearance => true}},
-  #     {"KALE" => {:price => 3.00, :clearance => false}},
-  #     {"BLACK_BEANS" => {:price => 2.50, :clearance => false}},
-  #     {"ALMONDS" => {:price => 9.00, :clearance => false}},
-  #     {"TEMPEH" => {:price => 3.00, :clearance => true}},
-  #     {"CHEESE" => {:price => 6.50, :clearance => false}},
-  #     {"BEER" => {:price => 13.00, :clearance => false}},
-  #     {"PEANUTBUTTER" => {:price => 3.00, :clearance => true}},
-  #     {"BEETS" => {:price => 2.50, :clearance => false}},
-  #     {"SOY MILK" => {:price => 4.50, :clearance => true}}
-  #   ]
-  # end
+  # let(:items) do
+    [
+      {"AVOCADO" => {:price => 3.00, :clearance => true}},
+      {"KALE" => {:price => 3.00, :clearance => false}},
+      {"BLACK_BEANS" => {:price => 2.50, :clearance => false}},
+      {"ALMONDS" => {:price => 9.00, :clearance => false}},
+      {"TEMPEH" => {:price => 3.00, :clearance => true}},
+      {"CHEESE" => {:price => 6.50, :clearance => false}},
+      {"BEER" => {:price => 13.00, :clearance => false}},
+      {"PEANUTBUTTER" => {:price => 3.00, :clearance => true}},
+      {"BEETS" => {:price => 2.50, :clearance => false}},
+      {"SOY MILK" => {:price => 4.50, :clearance => true}}
+    ]
+  end
   # describe "#consolidate_cart" do
   #   it "adds a count of one to each item when there are no duplicates" do
   #     cart = [find_item('TEMPEH'), find_item('PEANUTBUTTER'), find_item('ALMONDS')]
